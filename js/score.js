dbed9bf7571c8c92057e3b65744cab365c4ec3e6
pixel <pixelisprocrastinating15@gmail.com>
2026-09-03 16:07:04 +0200

    Update score.js

--- js/score.js
+++ js/score.js
@@ -7,7 +7,6 @@ const scale = 3;
  * Calculate the score awarded when having a certain percentage on a list level
  * @param {Number} rank Position on the list
  * @param {Number} percent Percentage of completion
- * @param {Number} minPercent Minimum percentage required
  * @returns {Number}
  */
 export function score(rank, percent=100) {
@@ -17,10 +16,7 @@ export function score(rank, percent=100) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = (-24.9975*Math.pow(rank-1, 0.4) + 200) *
-        ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
-
-    score = Math.max(0, score);
+    let score = Math.max(0, -24.9975*Math.pow(rank-1, 0.4) + 200);
 
     if (percent != 100) {
         return round(score - score / 3);
